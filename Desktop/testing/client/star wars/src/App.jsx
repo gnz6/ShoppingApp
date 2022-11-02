b9f1ba5dd0d738d5b13f2ddc29c59bf4808a94ff
Gonzalo <102692082+gnz6@users.noreply.github.com>
2022-11-02 20:12:12 -0300

    boler

--- Desktop/testing/client/star wars/src/App.jsx
+++ Desktop/testing/client/star wars/src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from './components/Landing'
+import axios from "axios";
+import Nav from './components/Nav';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const auth = JSON.parse(localStorage.getItem("token"))
+  if (auth) {
+    axios.defaults.headers.common['x-access-token'] = auth.token;
+} else {
+    axios.defaults.headers.common['x-access-token'] = null;
+}
 
   return (
+    <BrowserRouter>
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Routes>
+      <Route path='/' element={<Nav/>}/>
+      <Route path='landing' element={<Landing/>}/>
+    </Routes>
     </div>
+    </BrowserRouter>
   )
 }
 
