335fd530769f340624edcdb9d3b4f46abba04365
Gonzalo <102692082+gnz6@users.noreply.github.com>
2022-11-10 00:18:29 -0300

    fav

--- Desktop/STAR WARS/api/controllers/favs.js
+++ Desktop/STAR WARS/api/controllers/favs.js
@@ -27,13 +27,26 @@ const deleteFav = async(req,res)=>{
     try {
         const {id} = req.params;
         const {user_id} = req.body;
-        // if(!id || !user_id) return res.status(404).send("Need id and user to delete a favourite")
-         await peopleModel.findOne({_id:id});
-         await usersModel.findOneAndUpdate({_id:user_id},
-            {$pull: {favs: id}},
-            { new: true, useFindAndModify: false }
-            );
-        res.status(200).send("Character removed from favs");
+         if(!id || !user_id) return res.status(400).json({msg:"Need id and user to delete a favourite" , data: `user id : ${user_id} character id : ${id}`})
+        //  const character = await peopleModel.findOne({_id:id});
+        //  const user = await usersModel.findOneAndUpdate({_id:user_id},
+        //     {$pull: {favs: id}},
+        //     { new: true, useFindAndModify: false }
+        //     );
+        //     console.log(user);
+        const user = await usersModel.findOne({_id:user_id})
+       const character =  await peopleModel.findOne({_id:id})
+        if(!user) return res.status(400).send("User not found")
+        if(!character) return res.status(400).send("Character not found")
+        if(user && character){
+            const update = await usersModel.updateOne({_id:user_id},
+            {$pull:{favs: id}},
+            {new:true , useFindAndModify: false}
+            )
+            return res.status(200).send(update);
+        }
+
+        // res.status(200).send("Character removed from favs");
     } catch (error) {
         console.log(error);
         res.status(400).send("Cant delete this fav")
